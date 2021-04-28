cdfb98e28576874d052832ced2e19c6d1889c546
juliocesardemoraes <jcmcf2012@gmail.com>
2021-04-28 10:16:12 -0300

    Disabling Cors for all since request is from within the server

--- app.js
+++ app.js
@@ -37,11 +37,7 @@ client.connect();
 
 
 //WebSocket Socket.io 
-const io = require('socket.io')(server, {
-  cors: {
-    origin: '*',
-  }
-});
+const io = require('socket.io')(server);
 
 io.on('connection',function(socket){
   let requestSuccessful = false;
